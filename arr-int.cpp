2b6675c4799dca34afe995bc7899ed072ac3d92c
Thinh Le <55011202+ThinhLe514@users.noreply.github.com>
2020-03-18 15:54:48 -0400

    Add files via upload

--- arr-int.cpp
+++ arr-int.cpp
@@ -1,89 +1,91 @@
 #include <string>
 #include <cstdlib>
 #include <iostream>
+
 using namespace std;
 
-class DynamicArray 
+template <class T>
+class DynamicArray
 {
 public:
-  DynamicArray();
-  ~DynamicArray();
-  void addEntry(int newEntry);
-  int getEntry(int index);
-  int getSize();
+    DynamicArray();
+    ~DynamicArray();
+    void addEntry(T newEntry);
+    T getEntry(int index);
+    int getSize();
 
 private:
-  int *dynamicArray;
-  int size;
+    T *dynamicArray;
+    int size;
 };
 
-DynamicArray::DynamicArray()
+template <class T>
+DynamicArray<T>::DynamicArray()
 {
-  dynamicArray = nullptr;
-  size = 0;
+    dynamicArray = nullptr;
+    size = 0;
 }
 
-
-DynamicArray::~DynamicArray()
+template <class T>
+DynamicArray<T>::~DynamicArray()
 {
-  if (dynamicArray != nullptr)
-    delete[] dynamicArray;
+    if (dynamicArray != nullptr)
+        delete[] dynamicArray;
 }
 
-int DynamicArray::getSize()
+template <class T>
+int DynamicArray<T>::getSize()
 {
-  return size;
+    return size;
 }
 
-// Adds an entry to the dynamic array. 
-void DynamicArray::addEntry(int newEntry)
+template <class T>
+// Adds an entry to the dynamic array.
+void DynamicArray<T>::addEntry(T newEntry)
 {
     // Create a new array, copy the contents of the old array, then delete it
-    int *newArray = new int[size + 1];
+    T *newArray = new T[size + 1];
     for (int i = 0; i < size; i++)
     {
-    newArray[i] = dynamicArray[i];
+        newArray[i] = dynamicArray[i];
     }
-    
+
     delete[] dynamicArray;
-    
+
     dynamicArray = newArray;
-    
+
     // Add the new entry and increment the size
     newArray[size++] = newEntry;
 }
 
-
-
+template <class T>
 // Retrieve the element at a given index
-int DynamicArray::getEntry(int index)
+T DynamicArray<T>::getEntry(int index)
 {
-  if ((index < 0) || (index >= size)){
-    cout << "Out of the index !" << endl;
-    return 0;
-
-  }
-  return dynamicArray[index];
+    if ((index < 0) || (index >= size))
+    {
+        cout << "Out of the index !" << endl;
+        return 0;
+    }
+    return dynamicArray[index];
 }
-
 
 int main()
 {
-  DynamicArray x;
+    DynamicArray<string> x;
 
-  // Adding entries
-  x.addEntry(7);
-  x.addEntry(5);
-  x.addEntry(4);
-  x.addEntry(2);
-  x.addEntry(8);
+    // Adding entries
+    x.addEntry("Frank");
+    x.addEntry("Wiggum");
+    x.addEntry("Nahasapeemapetilon");
+    x.addEntry("Quimby");
+    x.addEntry("Flanders");
 
-  // Output entries
-  cout << "List:" << endl;
-  
-  for (int i = 0; i < x.getSize(); i++)
-    cout << x.getEntry(i) << endl;
-  
-  return 0;
+    // Output entries
+    cout << "List:" << endl;
+
+    for (int i = 0; i < x.getSize(); i++)
+        cout << x.getEntry(i) << endl;
+
+    return 0;
 }
-
